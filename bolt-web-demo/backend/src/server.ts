9c196ffc6b1e7ee619d03fcb8a321fb6e1970c7e
nicolas <48695862+merklefruit@users.noreply.github.com>
2024-04-24 17:53:02 +0200

    wip: web demo components

--- bolt-web-demo/backend/src/server.ts
+++ bolt-web-demo/backend/src/server.ts
@@ -2,8 +2,9 @@ import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
 import { json } from "body-parser";
+import { spawn } from "node:child_process";
 
-const PORT = 3001;
+const SERVER_PORT = 3001;
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
@@ -25,10 +26,10 @@ app.post("/events", (req, res) => {
     res.status(400).send("No message provided");
   } else {
     // Broadcast the message to all connected WebSocket clients
-    io.emit("new-event", message);
+    io.emit("new-event", { message, timestamp: new Date().toISOString() });
 
     res.setHeader("Content-Type", "text/plain");
-    res.send("OK, Event sent to all clients");
+    res.send("OK");
   }
 });
 
@@ -42,6 +43,50 @@ io.on("connection", (socket) => {
 });
 
 // Listen on the specified port
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+server.listen(SERVER_PORT, () => {
+  console.log(`Server is running on http://localhost:${SERVER_PORT}`);
 });
+
+async function waitForBeaconClientPort() {
+  const waitTimeMs = 2000;
+  let attemptsDone = 0;
+  const maxAttempts = 20;
+  let shouldTryAgain = true;
+
+  while (shouldTryAgain && attemptsDone < maxAttempts) {
+    console.log({ attemptsDone, maxAttempts });
+    try {
+      const kurtosisPort = spawn("kurtosis", [
+        "port",
+        "print",
+        "bolt-devnet",
+        "el-1-geth-lighthouse",
+        "rpc",
+      ]);
+
+      kurtosisPort.stdout.on("data", (data) => {
+        console.log(`stdout: ${data}`);
+        io.emit("new-event", {
+          message: data.toString(),
+          timestamp: new Date().toISOString(),
+        });
+        shouldTryAgain = false;
+      });
+
+      kurtosisPort.stderr.on("data", (data) => {
+        console.error(`stderr: ${data}`);
+      });
+
+      kurtosisPort.on("close", (code) => {
+        console.log(`child process exited with code ${code}`);
+      });
+    } catch (e) {
+      console.error(`Error while trying to get the port: ${e}`);
+    }
+
+    attemptsDone++;
+    await new Promise((resolve) => setTimeout(resolve, waitTimeMs));
+  }
+}
+
+waitForBeaconClientPort();
--- bolt-web-demo/frontend/src/app/page.tsx
+++ bolt-web-demo/frontend/src/app/page.tsx
@@ -1,24 +1,41 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { useState, useEffect } from "react";
 import io from "socket.io-client";
 
 export default function Home() {
   const [messages, setMessages] = useState<Array<any>>([]);
+  const [timerActive, setTimerActive] = useState<boolean>(false);
+  const [time, setTime] = useState(0);
 
   useEffect(() => {
     const newSocket = io("http://localhost:3001", { autoConnect: true });
 
     newSocket.on("new-event", (message) => {
       console.log("Message from server:", message);
-      setMessages((prev) => [...prev, message]);
+      setMessages((prev) => [message, ...prev]);
     });
 
     return () => {
       newSocket.close();
     };
   }, []);
+
+  useEffect(() => {
+    let interval: any = null;
+
+    if (timerActive) {
+      interval = setInterval(() => {
+        setTime((prev) => prev + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [timerActive]);
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
@@ -28,26 +45,99 @@ export default function Home() {
         <p>Your friendly preconfirmation companion.</p>
       </div>
 
-      <div className="w-full max-w-5xl pt-8">
+      <div className="w-full max-w-5xl pt-4">
         <div className="grid gap-3 border p-4 border-gray-800">
           <p className="text-lg">Step 1: send a transaction</p>
-          <small className="text-sm max-w-2xl">
+          <small className="text-sm max-w-3xl">
             By clicking this button you will create a transaction and send it as
             a preconfirmation request to the BOLT sidecar of the next proposer
-            in line:
+            in line. This transaction is crafted from a pre-funded account in
+            the devnet for demo purposes.
           </small>
-          <Button className="max-w-sm">Send</Button>
 
-          <div>
-            <h1>Messages from WebSocket</h1>
-            <ul>
+          <div className="flex flex-col items-center">
+            <Button
+              className="max-w-sm"
+              onClick={async () => {
+                // 0. fetch slot number
+
+                // 1. POST preconfirmation
+                try {
+                  const res = await fetch("http://localhost:9061", {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                      id: "1",
+                      jsonrpc: "2.0",
+                      method: "eth_requestPreconfirmation",
+                      params: [
+                        {
+                          slot: 1,
+                          txHash: "0x1234",
+                          rawTx: "0x1234",
+                        },
+                      ],
+                    }),
+                  }).then((res) => res.json());
+
+                  console.log("res", res);
+                } catch (e) {
+                  console.error(e);
+                }
+
+                // 2. start timer to wait for confirmation from BOLT
+                setTimerActive(true);
+                setTime(0);
+              }}
+            >
+              Send Preconfirmation
+            </Button>
+          </div>
+        </div>
+
+        <div className="grid gap-3 border p-4 border-gray-800 mt-4">
+          <p className="text-lg">
+            Step 2: wait for a cryptoeconomic preconfirmation
+          </p>
+          <small className="text-sm max-w-3xl">
+            The transaction will be processed by BOLT and you will receive a
+            preconfirmation for inclusion in the next block.
+          </small>
+
+          {timerActive && (
+            <div>
+              <p>
+                Waiting for preconfirmation. Time elapsed: <b>{time}</b>ms
+              </p>
+            </div>
+          )}
+        </div>
+
+        <div className="grid gap-3 border p-4 border-gray-800 mt-4">
+          <p className="text-lg">Event logs</p>
+          <small className="text-sm max-w-3xl">
+            This is the list of events received from the server.
+          </small>
+
+          <ScrollArea className="max-h-80">
+            <ul className="font-mono">
               {messages.map((message, index) => (
-                <li key={index}>{message}</li>
+                <li key={index}>
+                  <span>{parseDateToMs(message.timestamp)}</span>
+                  {" | "}
+                  {JSON.stringify(message.message)}
+                </li>
               ))}
             </ul>
-          </div>
+          </ScrollArea>
         </div>
       </div>
     </main>
   );
+}
+
+function parseDateToMs(date: any) {
+  return new Date(date).toISOString().split("T")[1].split("Z")[0];
 }
