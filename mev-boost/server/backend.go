67612fbad9da79d3f151175c0fad09dd63c47861
Jonas Bostoen <jonas.bostoen@hotmail.com>
2024-05-01 14:22:34 +0200

    feat(server): add submitConstraints proxy functionality

--- mev-boost/server/backend.go
+++ mev-boost/server/backend.go
@@ -4,6 +4,7 @@ const (
 	// Router paths
 	pathStatus            = "/eth/v1/builder/status"
 	pathRegisterValidator = "/eth/v1/builder/validators"
+	pathSubmitConstraint  = "/eth/v1/builder/constraints"
 	pathGetHeader         = "/eth/v1/builder/header/{slot:[0-9]+}/{parent_hash:0x[a-fA-F0-9]+}/{pubkey:0x[a-fA-F0-9]+}"
 	pathGetPayload        = "/eth/v1/builder/blinded_blocks"
 
