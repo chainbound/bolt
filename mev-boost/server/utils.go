d8f1ba419ecad602088d5fb9224872431aa6d5fe
Jonas Bostoen <jonas.bostoen@hotmail.com>
2024-05-01 11:29:39 +0200

    feat(server): add constraint cache, refactor proof verification

--- mev-boost/server/utils.go
+++ mev-boost/server/utils.go
@@ -270,13 +270,3 @@ func getPayloadResponseIsEmpty(payload *builderApi.VersionedSubmitBlindedBlockRe
 	}
 	return false
 }
-
-func filter[T any](slice []T, f func(T) bool) []T {
-	var n []T
-	for _, e := range slice {
-		if f(e) {
-			n = append(n, e)
-		}
-	}
-	return n
-}
