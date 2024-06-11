db3ade70bd753e1b4c340b773318b95633510273
Jonas Bostoen <jonas.bostoen@hotmail.com>
2024-06-11 23:44:32 +0200

    feat(sidecar): commit boost signature tests

--- bolt-sidecar/src/client/commit_boost.rs
+++ bolt-sidecar/src/client/commit_boost.rs
@@ -2,7 +2,6 @@ use alloy_rpc_types_beacon::{BlsPublicKey, BlsSignature};
 use cb_common::pbs::{COMMIT_BOOST_API, PUBKEYS_PATH, SIGN_REQUEST_PATH};
 use cb_crypto::types::SignRequest;
 use ethereum_consensus::ssz::prelude::HashTreeRoot;
-use reqwest::{IntoUrl, Url};
 use thiserror::Error;
 
 use crate::primitives::ConstraintsMessage;
@@ -10,7 +9,7 @@ use crate::primitives::ConstraintsMessage;
 const id: &str = "bolt";
 
 pub struct CommitBoostClient {
-    url: Url,
+    url: String,
     client: reqwest::Client,
     pubkeys: Vec<BlsPublicKey>,
 }
@@ -26,9 +25,9 @@ pub enum CommitBoostError {
 }
 
 impl CommitBoostClient {
-    pub async fn new(url: impl IntoUrl) -> Result<Self, CommitBoostError> {
+    pub async fn new(url: impl Into<String>) -> Result<Self, CommitBoostError> {
         let mut client = Self {
-            url: url.into_url().map_err(|_| CommitBoostError::BadUrl)?,
+            url: url.into(),
             client: reqwest::Client::new(),
             pubkeys: Vec::new(),
         };
@@ -87,5 +86,70 @@ impl CommitBoostClient {
         }
 
         serde_json::from_slice(&response_bytes).expect("failed deser")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use alloy_node_bindings::{Anvil, AnvilInstance};
+    use alloy_primitives::{hex, Address, U256};
+    use alloy_provider::network::{EthereumSigner, TransactionBuilder};
+    use alloy_rpc_types::TransactionRequest;
+    use alloy_signer::SignerSync;
+    use alloy_signer_wallet::LocalWallet;
+
+    use crate::primitives::InclusionRequest;
+
+    use super::*;
+
+    fn launch_anvil() -> AnvilInstance {
+        Anvil::new().block_time(1).chain_id(1337).spawn()
+    }
+
+    #[tokio::test]
+    async fn test_commit_boost_signature() {
+        let _ = tracing_subscriber::fmt::try_init();
+
+        let anvil = launch_anvil();
+
+        let wallet: LocalWallet = anvil.keys()[0].clone().into();
+
+        let sender = anvil.addresses()[0];
+
+        let client = CommitBoostClient::new("http://localhost:33950")
+            .await
+            .unwrap();
+
+        let tx_request = default_transaction(sender);
+
+        let sig = wallet.sign_message_sync(&hex!("abcd")).unwrap();
+
+        let signer: EthereumSigner = wallet.into();
+        let signed = tx_request.build(&signer).await.unwrap();
+
+        let req = InclusionRequest {
+            slot: 20,
+            tx: signed,
+            signature: sig,
+        };
+
+        let message = ConstraintsMessage::build(0, 0, req).unwrap();
+        let signature = client.sign_constraint(&message).await.unwrap();
+
+        println!("Message signed, signature: {signature}")
+        // assert!(signature.verify(&message.hash_tree_root(), &client.pubkeys.first().unwrap()));
+    }
+
+    fn default_transaction(sender: Address) -> TransactionRequest {
+        TransactionRequest::default()
+            .with_from(sender)
+            // Burn it
+            .with_to(Address::random())
+            .with_chain_id(1337)
+            .with_nonce(0)
+            .with_value(U256::from(100))
+            .with_gas_limit(21_000)
+            .with_max_priority_fee_per_gas(1_000_000_000)
+            .with_max_fee_per_gas(20_000_000_000)
     }
 }
