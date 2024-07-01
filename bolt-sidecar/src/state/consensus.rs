4c0e475571407d3586bd7e2e64ae57bba482804a
Naman Garg <0708ng@gmail.com>
2024-07-01 11:58:02 +0530

    feat(sidecar): fetch proposer duties

--- bolt-sidecar/src/state/consensus.rs
+++ bolt-sidecar/src/state/consensus.rs
@@ -2,7 +2,7 @@
 #![allow(unused_variables)]
 #![allow(missing_debug_implementations)]
 
-use beacon_api_client::{mainnet::Client, BlockId};
+use beacon_api_client::{mainnet::Client, BlockId, ProposerDuty};
 use ethereum_consensus::deneb::BeaconBlockHeader;
 use reqwest::Url;
 
@@ -14,9 +14,16 @@ pub enum ConsensusError {
     BeaconApiError(#[from] beacon_api_client::Error),
 }
 
+pub struct Epoch {
+    pub value: u64,
+    pub start_slot: u64,
+    pub proposer_duties: Vec<ProposerDuty>,
+}
+
 pub struct ConsensusState {
     beacon_api_client: Client,
     header: BeaconBlockHeader,
+    epoch: Epoch,
 }
 
 impl ConsensusState {
@@ -28,6 +35,11 @@ impl ConsensusState {
         Self {
             beacon_api_client,
             header: BeaconBlockHeader::default(),
+            epoch: Epoch {
+                value: 0,
+                start_slot: 0,
+                proposer_duties: vec![],
+            },
         }
     }
 
@@ -40,6 +52,24 @@ impl ConsensusState {
 
         self.header = update.header.message;
 
+        let slot = self.header.slot;
+        let epoch = slot / 32;
+
+        if epoch != self.epoch.value {
+            self.epoch.value = epoch;
+            self.epoch.start_slot = epoch * 32;
+
+            self.fetch_proposer_duties(epoch).await?;
+        }
+
+        Ok(())
+    }
+
+    /// Fetch proposer duties for the given epoch.
+    async fn fetch_proposer_duties(&mut self, epoch: u64) -> Result<(), ConsensusError> {
+        let duties = self.beacon_api_client.get_proposer_duties(epoch).await?;
+
+        self.epoch.proposer_duties = duties.1;
         Ok(())
     }
 }
