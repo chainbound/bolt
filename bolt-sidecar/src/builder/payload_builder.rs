a4dda2e2ed893977a8f12efa0954e3c6a6371bd0
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2024-08-21 13:06:11 +0200

    chore(sidecar): fallback block builder logs review

--- bolt-sidecar/src/builder/payload_builder.rs
+++ bolt-sidecar/src/builder/payload_builder.rs
@@ -112,24 +112,24 @@ impl FallbackPayloadBuilder {
         // For the timestamp, we must use the one expected by the beacon chain instead, to
         // prevent edge cases where the proposer before us has missed their slot.
         let latest_block = self.execution_rpc_client.get_block(None, true).await?;
-        debug!(num = ?latest_block.header.number, "got latest block");
+        trace!(num = ?latest_block.header.number, "got latest block");
 
         let withdrawals = self.get_expected_withdrawals_at_head().await?;
-        debug!(amount = ?withdrawals.len(), "got expected withdrawals");
+        trace!(amount = ?withdrawals.len(), "got expected withdrawals");
 
         let prev_randao = self.get_prev_randao().await?;
-        debug!(randao = ?prev_randao, "got prev_randao");
+        trace!(randao = ?prev_randao, "got prev_randao");
 
         let parent_beacon_block_root =
             self.beacon_api_client.get_beacon_block_root(BlockId::Head).await?;
-        debug!(parent = ?parent_beacon_block_root, "got parent_beacon_block_root");
+        trace!(parent = ?parent_beacon_block_root, "got parent_beacon_block_root");
 
         let versioned_hashes = transactions
             .iter()
             .flat_map(|tx| tx.blob_versioned_hashes())
             .flatten()
             .collect::<Vec<_>>();
-        info!(amount = ?versioned_hashes.len(), "got versioned_hashes");
+        trace!(amount = ?versioned_hashes.len(), "got versioned_hashes");
 
         let base_fee = calc_next_block_base_fee(
             latest_block.header.gas_used,
@@ -189,11 +189,11 @@ impl FallbackPayloadBuilder {
                 .fetch_next_payload_hint(&exec_payload, &versioned_hashes, parent_beacon_block_root)
                 .await?;
 
-            debug!("engine_hint: {:?}", engine_hint);
+            trace!("engine_hint: {:?}", engine_hint);
 
             match engine_hint {
                 EngineApiHint::BlockHash(hash) => {
-                    warn!("Should not receive block hash hint {:?}", hash);
+                    trace!("Should not receive block hash hint {:?}", hash);
                     hints.block_hash = Some(hash)
                 }
 
@@ -454,9 +454,9 @@ mod tests {
         let raw_encoded = tx_signed.encoded_2718();
         let tx_signed_reth = TransactionSigned::decode_enveloped(&mut raw_encoded.as_slice())?;
 
-        let slot = genesis_time +
-            (SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs() / cfg.chain.slot_time()) +
-            1;
+        let slot = genesis_time
+            + (SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs() / cfg.chain.slot_time())
+            + 1;
 
         let block = builder.build_fallback_payload(slot, &[tx_signed_reth]).await?;
         assert_eq!(block.body.len(), 1);
