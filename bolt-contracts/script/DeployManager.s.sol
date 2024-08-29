8744b3d71bca551123dfadebf6575b7b8ceb926e
nicolas <48695862+merklefruit@users.noreply.github.com>
2024-08-29 13:32:16 +0200

    chore: solc version bump

--- bolt-contracts/script/DeployManager.s.sol
+++ bolt-contracts/script/DeployManager.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.25;
 
 import {Script, console} from "forge-std/Script.sol";
 
--- bolt-contracts/src/lib/ssz/ValidatorProver.sol
+++ bolt-contracts/src/lib/ssz/ValidatorProver.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.25;
 
 import {SSZ} from "./SSZ.sol";
 import {SSZContainers} from "./SSZContainers.sol";
