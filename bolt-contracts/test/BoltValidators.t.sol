13a64c05e46d357fced9106eb296403cc38326cc
nicolas <48695862+merklefruit@users.noreply.github.com>
2024-09-03 11:22:14 +0200

    chore: unit test

--- bolt-contracts/test/BoltValidators.t.sol
+++ bolt-contracts/test/BoltValidators.t.sol
@@ -3,8 +3,11 @@ pragma solidity 0.8.25;
 
 import {Test, console} from "forge-std/Test.sol";
 import {BoltValidators} from "../src/contracts/BoltValidators.sol";
+import {BLS12381} from "../src/lib/bls/BLS12381.sol";
 
 contract BoltValidatorsTest is Test {
+    using BLS12381 for BLS12381.G1Point;
+
     BoltValidators public validators;
 
     uint64[] public validatorIndexes;
@@ -18,12 +21,23 @@ contract BoltValidatorsTest is Test {
         validators = new BoltValidators(admin);
 
         // Give some ether to the accounts for gas
+        vm.deal(admin, 20 ether);
         vm.deal(provider, 20 ether);
         vm.deal(operator, 20 ether);
         vm.deal(validator, 20 ether);
     }
 
-    function testRegistration() public {
-        // TODO
+    function testUnsafeRegistration() public {
+        // pubkeys aren't checked, any point will be fine
+        BLS12381.G1Point memory pubkey = BLS12381.generatorG1();
+
+        vm.prank(validator);
+        validators.registerValidatorUnsafe(pubkey, provider, operator);
+
+        BoltValidators.Validator memory registered = validators.getValidatorByPubkey(pubkey);
+        assertEq(registered.exists, true);
+        assertEq(registered.authorizedCollateralProvider, provider);
+        assertEq(registered.authorizedOperator, operator);
+        assertEq(registered.controller, validator);
     }
 }
