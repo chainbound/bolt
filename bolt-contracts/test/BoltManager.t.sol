c818ce128b1e801bb0b3b45e225bab561a66e12d
nicolas <48695862+merklefruit@users.noreply.github.com>
2024-09-03 13:47:11 +0200

    feat: test fixture for symbiotic core

--- bolt-contracts/test/BoltManager.t.sol
+++ bolt-contracts/test/BoltManager.t.sol
@@ -2,32 +2,94 @@
 pragma solidity 0.8.25;
 
 import {Test, console} from "forge-std/Test.sol";
+
+import {INetworkRegistry} from "@symbiotic/interfaces/INetworkRegistry.sol";
+import {IOperatorRegistry} from "@symbiotic/interfaces/IOperatorRegistry.sol";
+
 import {BoltValidators} from "../src/contracts/BoltValidators.sol";
 import {BoltManager} from "../src/contracts/BoltManager.sol";
+import {BLS12381} from "../src/lib/bls/BLS12381.sol";
+
+import {SymbioticSetupFixture} from "./fixtures/SymbioticSetup.f.sol";
 
 contract BoltManagerTest is Test {
+    using BLS12381 for BLS12381.G1Point;
+
     BoltValidators public validators;
     BoltManager public manager;
 
     uint64[] public validatorIndexes;
 
-    address admin = address(0x1);
-    address provider = address(0x2);
-    address operator = address(0x3);
-    address validator = address(0x4);
-    address symbioticNetwork = address(0x5);
+    address public networkRegistry;
+    address public operatorRegistry;
+    address public operatorMetadataService;
+    address public networkMetadataService;
+    address public networkMiddlewareService;
+    address public operatorVaultOptInService;
+    address public operatorNetworkOptInService;
+    address public vetoSlasherImpl;
+    address public vaultImpl;
+    address public networkRestakeDelegatorImpl;
+
+    address deployer = address(0x1);
+    address admin = address(0x2);
+    address provider = address(0x3);
+    address operator = address(0x4);
+    address validator = address(0x5);
+    address networkAdmin = address(0x6);
 
     function setUp() public {
-        validators = new BoltValidators(admin);
-        manager = new BoltManager(address(validators), symbioticNetwork);
-
         // Give some ether to the accounts for gas
+        vm.deal(deployer, 200 ether);
+        vm.deal(admin, 20 ether);
         vm.deal(provider, 20 ether);
         vm.deal(operator, 20 ether);
         vm.deal(validator, 20 ether);
+        vm.deal(networkAdmin, 20 ether);
+
+        // Deploy Symbiotic contracts
+        (
+            ,
+            ,
+            ,
+            networkRegistry,
+            operatorRegistry,
+            operatorMetadataService,
+            networkMetadataService,
+            networkMiddlewareService,
+            operatorVaultOptInService,
+            operatorNetworkOptInService,
+            ,
+            vetoSlasherImpl,
+            ,
+            vaultImpl,
+            networkRestakeDelegatorImpl,
+        ) = new SymbioticSetupFixture().setup(deployer, admin);
+
+        // Register the network in Symbiotic
+        vm.prank(networkAdmin);
+        INetworkRegistry(networkRegistry).registerNetwork();
+
+        // Deploy Bolt contracts
+        validators = new BoltValidators(admin);
+        manager = new BoltManager(address(validators), networkAdmin);
     }
 
-    function testRegistration() public {
-        // TODO
+    function testFullSymbioticOptIn() public {
+        // --- 1. register Validator in BoltValidators ---
+
+        // pubkeys aren't checked, any point will be fine
+        BLS12381.G1Point memory pubkey = BLS12381.generatorG1();
+
+        vm.prank(validator);
+        validators.registerValidatorUnsafe(pubkey, provider, operator);
+        assertEq(validators.getValidatorByPubkey(pubkey).exists, true);
+        assertEq(validators.getValidatorByPubkey(pubkey).authorizedOperator, operator);
+        assertEq(validators.getValidatorByPubkey(pubkey).authorizedCollateralProvider, provider);
+
+        // --- 2. register Operator in Symbiotic ---
+
+        vm.prank(operator);
+        IOperatorRegistry(operatorRegistry).registerOperator();
     }
 }
---
+++ bolt-contracts/test/fixtures/SymbioticSetup.f.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {Script, console2} from "forge-std/Script.sol";
+
+import {VaultFactory} from "@symbiotic/contracts/VaultFactory.sol";
+import {DelegatorFactory} from "@symbiotic/contracts/DelegatorFactory.sol";
+import {SlasherFactory} from "@symbiotic/contracts/SlasherFactory.sol";
+import {NetworkRegistry} from "@symbiotic/contracts/NetworkRegistry.sol";
+import {OperatorRegistry} from "@symbiotic/contracts/OperatorRegistry.sol";
+import {MetadataService} from "@symbiotic/contracts/service/MetadataService.sol";
+import {NetworkMiddlewareService} from "@symbiotic/contracts/service/NetworkMiddlewareService.sol";
+import {OptInService} from "@symbiotic/contracts/service/OptInService.sol";
+
+import {Vault} from "@symbiotic/contracts/vault/Vault.sol";
+import {NetworkRestakeDelegator} from "@symbiotic/contracts/delegator/NetworkRestakeDelegator.sol";
+import {FullRestakeDelegator} from "@symbiotic/contracts/delegator/FullRestakeDelegator.sol";
+import {Slasher} from "@symbiotic/contracts/slasher/Slasher.sol";
+import {VetoSlasher} from "@symbiotic/contracts/slasher/VetoSlasher.sol";
+
+import {VaultConfigurator} from "@symbiotic/contracts/VaultConfigurator.sol";
+
+contract SymbioticSetupFixture is Script {
+    function setup(
+        address deployer,
+        address owner
+    )
+        public
+        returns (
+            address vaultFactory,
+            address delegatorFactory,
+            address slasherFactory,
+            address networkRegistry,
+            address operatorRegistry,
+            address operatorMetadataService,
+            address networkMetadataService,
+            address networkMiddlewareService,
+            address operatorVaultOptInService,
+            address operatorNetworkOptInService,
+            address slasherImpl,
+            address vetoSlasherImpl,
+            address vaultConfigurator,
+            address vaultImpl,
+            address networkRestakeDelegatorImpl,
+            address fullRestakeDelegatorImpl
+        )
+    {
+        vm.startPrank(deployer);
+
+        VaultFactory vaultFactory = new VaultFactory(deployer);
+        DelegatorFactory delegatorFactory = new DelegatorFactory(deployer);
+        SlasherFactory slasherFactory = new SlasherFactory(deployer);
+        NetworkRegistry networkRegistry = new NetworkRegistry();
+        OperatorRegistry operatorRegistry = new OperatorRegistry();
+        MetadataService operatorMetadataService = new MetadataService(address(operatorRegistry));
+        MetadataService networkMetadataService = new MetadataService(address(networkRegistry));
+        NetworkMiddlewareService networkMiddlewareService = new NetworkMiddlewareService(address(networkRegistry));
+        OptInService operatorVaultOptInService = new OptInService(address(operatorRegistry), address(vaultFactory));
+        OptInService operatorNetworkOptInService = new OptInService(address(operatorRegistry), address(networkRegistry));
+
+        address vaultImpl =
+            address(new Vault(address(delegatorFactory), address(slasherFactory), address(vaultFactory)));
+        vaultFactory.whitelist(vaultImpl);
+
+        address networkRestakeDelegatorImpl = address(
+            new NetworkRestakeDelegator(
+                address(networkRegistry),
+                address(vaultFactory),
+                address(operatorVaultOptInService),
+                address(operatorNetworkOptInService),
+                address(delegatorFactory),
+                delegatorFactory.totalTypes()
+            )
+        );
+        delegatorFactory.whitelist(networkRestakeDelegatorImpl);
+
+        address fullRestakeDelegatorImpl = address(
+            new FullRestakeDelegator(
+                address(networkRegistry),
+                address(vaultFactory),
+                address(operatorVaultOptInService),
+                address(operatorNetworkOptInService),
+                address(delegatorFactory),
+                delegatorFactory.totalTypes()
+            )
+        );
+        delegatorFactory.whitelist(fullRestakeDelegatorImpl);
+
+        address slasherImpl = address(
+            new Slasher(
+                address(vaultFactory),
+                address(networkMiddlewareService),
+                address(slasherFactory),
+                slasherFactory.totalTypes()
+            )
+        );
+        slasherFactory.whitelist(slasherImpl);
+
+        address vetoSlasherImpl = address(
+            new VetoSlasher(
+                address(vaultFactory),
+                address(networkMiddlewareService),
+                address(networkRegistry),
+                address(slasherFactory),
+                slasherFactory.totalTypes()
+            )
+        );
+        slasherFactory.whitelist(vetoSlasherImpl);
+
+        VaultConfigurator vaultConfigurator =
+            new VaultConfigurator(address(vaultFactory), address(delegatorFactory), address(slasherFactory));
+
+        vaultFactory.transferOwnership(owner);
+        delegatorFactory.transferOwnership(owner);
+        slasherFactory.transferOwnership(owner);
+
+        console2.log("VaultFactory: ", address(vaultFactory));
+        console2.log("DelegatorFactory: ", address(delegatorFactory));
+        console2.log("SlasherFactory: ", address(slasherFactory));
+        console2.log("NetworkRegistry: ", address(networkRegistry));
+        console2.log("OperatorRegistry: ", address(operatorRegistry));
+        console2.log("OperatorMetadataService: ", address(operatorMetadataService));
+        console2.log("NetworkMetadataService: ", address(networkMetadataService));
+        console2.log("NetworkMiddlewareService: ", address(networkMiddlewareService));
+        console2.log("OperatorVaultOptInService: ", address(operatorVaultOptInService));
+        console2.log("OperatorNetworkOptInService: ", address(operatorNetworkOptInService));
+        console2.log("VaultConfigurator: ", address(vaultConfigurator));
+
+        vm.stopPrank();
+
+        return (
+            address(vaultFactory),
+            address(delegatorFactory),
+            address(slasherFactory),
+            address(networkRegistry),
+            address(operatorRegistry),
+            address(operatorMetadataService),
+            address(networkMetadataService),
+            address(networkMiddlewareService),
+            address(operatorVaultOptInService),
+            address(operatorNetworkOptInService),
+            address(slasherImpl),
+            address(vetoSlasherImpl),
+            address(vaultConfigurator),
+            address(vaultImpl),
+            address(networkRestakeDelegatorImpl),
+            address(fullRestakeDelegatorImpl)
+        );
+    }
+}
