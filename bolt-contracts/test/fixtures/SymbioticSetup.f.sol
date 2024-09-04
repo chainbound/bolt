303ba3c1a1a0e5f5cc39c04b1389c80d9b6d898c
nicolas <48695862+merklefruit@users.noreply.github.com>
2024-09-04 15:52:40 +0200

    chore: nits

--- bolt-contracts/test/fixtures/SymbioticSetup.f.sol
+++ bolt-contracts/test/fixtures/SymbioticSetup.f.sol
@@ -17,7 +17,6 @@ import {NetworkRestakeDelegator} from "@symbiotic/contracts/delegator/NetworkRes
 import {FullRestakeDelegator} from "@symbiotic/contracts/delegator/FullRestakeDelegator.sol";
 import {Slasher} from "@symbiotic/contracts/slasher/Slasher.sol";
 import {VetoSlasher} from "@symbiotic/contracts/slasher/VetoSlasher.sol";
-import {IVaultConfigurator} from "@symbiotic/interfaces/IVaultConfigurator.sol";
 import {VaultConfigurator} from "@symbiotic/contracts/VaultConfigurator.sol";
 
 contract SymbioticSetupFixture is Test {
@@ -129,86 +128,5 @@ contract SymbioticSetupFixture is Test {
             address(networkRestakeDelegatorImpl_),
             address(fullRestakeDelegatorImpl_)
         );
-    }
-
-    function configureVault(
-        address vaultConfigurator,
-        IVaultConfigurator.InitParams memory params
-    ) public returns (address vault) {
-        (address vault_, address networkRestakeDelegator_, address slasher_) =
-            IVaultConfigurator(vaultConfigurator).create(params);
-
-        // IVaultConfigurator.InitParams({
-        //     version: 1,
-        //     owner: owner_,
-        //     vaultParams: IVault.InitParams({
-        //         collateral: address(collateral),
-        //         delegator: address(0),
-        //         slasher: address(0),
-        //         burner: burner,
-        //         epochDuration: epochDuration,
-        //         depositWhitelist: depositWhitelist,
-        //         isDepositLimit: isDepositLimit,
-        //         depositLimit: depositLimit,
-        //         defaultAdminRoleHolder: address(100),
-        //         depositWhitelistSetRoleHolder: address(99),
-        //         depositorWhitelistRoleHolder: address(101),
-        //         isDepositLimitSetRoleHolder: address(102),
-        //         depositLimitSetRoleHolder: address(103)
-        //     }),
-        //     delegatorIndex: 0,
-        //     delegatorParams: abi.encode(
-        //         INetworkRestakeDelegator.InitParams({
-        //             baseParams: IBaseDelegator.BaseParams({
-        //                 defaultAdminRoleHolder: address(104),
-        //                 hook: hook,
-        //                 hookSetRoleHolder: address(105)
-        //             }),
-        //             networkLimitSetRoleHolders: networkLimitSetRoleHolders,
-        //             operatorNetworkSharesSetRoleHolders: operatorNetworkSharesSetRoleHolders
-        //         })
-        //     ),
-        //     withSlasher: withSlasher,
-        //     slasherIndex: 0,
-        //     slasherParams: ""
-        // })
-
-        // vault = Vault(vault_);
-        // networkRestakeDelegator = NetworkRestakeDelegator(networkRestakeDelegator_);
-        // slasher = Slasher(slasher_);
-
-        // assertEq(vault.owner(), owner_);
-        // assertEq(vault.collateral(), address(collateral));
-        // assertEq(vault.delegator(), networkRestakeDelegator_);
-        // assertEq(vault.slasher(), withSlasher ? slasher_ : address(0));
-        // assertEq(vault.burner(), burner);
-        // assertEq(vault.epochDuration(), epochDuration);
-        // assertEq(vault.depositWhitelist(), depositWhitelist);
-        // assertEq(vault.isDepositLimit(), isDepositLimit);
-        // assertEq(vault.depositLimit(), depositLimit);
-        // assertEq(vault.hasRole(vault.DEFAULT_ADMIN_ROLE(), address(100)), true);
-        // assertEq(vault.hasRole(vault.DEPOSIT_WHITELIST_SET_ROLE(), address(99)), true);
-        // assertEq(vault.hasRole(vault.DEPOSITOR_WHITELIST_ROLE(), address(101)), true);
-        // assertEq(vault.hasRole(vault.IS_DEPOSIT_LIMIT_SET_ROLE(), address(102)), true);
-        // assertEq(vault.hasRole(vault.DEPOSIT_LIMIT_SET_ROLE(), address(103)), true);
-
-        // assertEq(networkRestakeDelegator.vault(), vault_);
-        // assertEq(networkRestakeDelegator.hasRole(networkRestakeDelegator.DEFAULT_ADMIN_ROLE(), address(104)), true);
-        // assertEq(networkRestakeDelegator.hook(), hook);
-        // assertEq(networkRestakeDelegator.hasRole(networkRestakeDelegator.HOOK_SET_ROLE(), address(105)), true);
-        // assertEq(networkRestakeDelegator.hasRole(networkRestakeDelegator.NETWORK_LIMIT_SET_ROLE(), address(106)), true);
-        // assertEq(
-        //     networkRestakeDelegator.hasRole(networkRestakeDelegator.OPERATOR_NETWORK_SHARES_SET_ROLE(), address(107)),
-        //     true
-        // );
-
-        // if (withSlasher) {
-        //     assertEq(slasher.vault(), vault_);
-        // }
-
-        assertEq(networkRestakeDelegator_, address(Vault(vault_).delegator()));
-        assertEq(slasher_, address(Vault(vault_).slasher()));
-
-        return vault_;
     }
 }
