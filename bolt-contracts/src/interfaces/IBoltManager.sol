f409562e54793531260ee1318df8ee2ec5b8d1d4
nicolas <48695862+merklefruit@users.noreply.github.com>
2024-09-06 17:40:06 +0200

    chore: updated interface

--- bolt-contracts/src/interfaces/IBoltManager.sol
+++ bolt-contracts/src/interfaces/IBoltManager.sol
@@ -21,24 +21,52 @@ interface IBoltManager {
     error NotVault();
     error CollateralNotWhitelisted();
 
+    function getEpochStartTs(uint48 epoch) external view returns (uint48);
+
+    function getEpochAtTs(uint48 timestamp) external view returns (uint48);
+
+    function getCurrentEpoch() external view returns (uint48);
+
+    function addWhitelistedCollateral(address collateral) external;
+
+    function removeWhitelistedCollateral(address collateral) external;
+
+    function getWhitelistedCollaterals() external view returns (address[] memory);
+
+    function isCollateralWhitelisted(address collateral) external view returns (bool);
+
     function registerSymbioticOperator(address operator) external;
 
     function pauseSymbioticOperator() external;
+
+    function unpauseSymbioticOperator() external;
 
     function registerSymbioticVault(address vault) external;
 
     function pauseSymbioticVault() external;
 
+    function unpauseSymbioticVault() external;
+
+    function isSymbioticVaultEnabled(address vault) external view returns (bool);
+
     function isSymbioticOperatorEnabled(address operator) external view returns (bool);
+
+    function getProposersStatus(bytes32[] memory pubkeyHashes) external view returns (ProposerStatus[] memory);
+
+    function getProposerStatus(bytes32 pubkeyHash) external view returns (ProposerStatus memory);
 
     function isSymbioticOperatorAuthorizedForValidator(
         address operator,
         bytes32 pubkeyHash
     ) external view returns (bool);
 
+    function getSymbioticOperatorStake(address operator, address collateral) external view returns (uint256);
+
     function getSymbioticOperatorStakeAt(
         address operator,
         address collateral,
         uint48 timestamp
     ) external view returns (uint256);
+
+    function getSymbioticTotalStake(uint48 epoch, address collateral) external view returns (uint256);
 }
