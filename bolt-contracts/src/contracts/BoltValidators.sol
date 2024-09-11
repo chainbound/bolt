5c5b30433d3f262d56c41e932f2e8af21b1ccc6a
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2024-09-11 14:16:20 +0200

    chore(contracts): forge fmt

--- bolt-contracts/src/contracts/BoltValidators.sol
+++ bolt-contracts/src/contracts/BoltValidators.sol
@@ -47,7 +47,9 @@ contract BoltValidators is IBoltValidators, BLSSignatureVerifier, Ownable {
 
     /// @notice Constructor
     /// @param _owner Address of the owner of the contract
-    constructor(address _owner) Ownable(_owner) {}
+    constructor(
+        address _owner
+    ) Ownable(_owner) {}
 
     // ========= ADMIN FUNCTIONS =========
 
@@ -117,12 +119,7 @@ contract BoltValidators is IBoltValidators, BLSSignatureVerifier, Ownable {
             revert UnsafeRegistrationNotAllowed();
         }
 
-        _registerValidator(
-            pubkey,
-            nextValidatorSequenceNumber,
-            authorizedCollateralProvider,
-            authorizedOperator
-        );
+        _registerValidator(pubkey, nextValidatorSequenceNumber, authorizedCollateralProvider, authorizedOperator);
     }
 
     /// @notice Register a single Validator and authorize a Collateral Provider and Operator for it
@@ -138,21 +135,12 @@ contract BoltValidators is IBoltValidators, BLSSignatureVerifier, Ownable {
         address authorizedCollateralProvider,
         address authorizedOperator
     ) public {
-        bytes memory message = abi.encodePacked(
-            block.chainid,
-            msg.sender,
-            nextValidatorSequenceNumber
-        );
+        bytes memory message = abi.encodePacked(block.chainid, msg.sender, nextValidatorSequenceNumber);
         if (!_verifySignature(message, signature, pubkey)) {
             revert InvalidAuthorizedCollateralProvider();
         }
 
-        _registerValidator(
-            pubkey,
-            nextValidatorSequenceNumber,
-            authorizedCollateralProvider,
-            authorizedOperator
-        );
+        _registerValidator(pubkey, nextValidatorSequenceNumber, authorizedCollateralProvider, authorizedOperator);
     }
 
     /// @notice Register a batch of Validators and authorize a Collateral Provider and Operator for them
@@ -168,23 +156,15 @@ contract BoltValidators is IBoltValidators, BLSSignatureVerifier, Ownable {
         address authorizedOperator
     ) public {
         uint256 validatorsCount = pubkeys.length;
-        uint64[] memory expectedValidatorSequenceNumbers = new uint64[](
-            validatorsCount
-        );
+        uint64[] memory expectedValidatorSequenceNumbers = new uint64[](validatorsCount);
         for (uint256 i = 0; i < validatorsCount; i++) {
-            expectedValidatorSequenceNumbers[i] =
-                nextValidatorSequenceNumber +
-                uint64(i);
+            expectedValidatorSequenceNumbers[i] = nextValidatorSequenceNumber + uint64(i);
         }
 
         // Reconstruct the unique message for which we expect an aggregated signature.
         // We need the msg.sender to prevent a front-running attack by an EOA that may
         // try to register the same validators
-        bytes memory message = abi.encodePacked(
-            block.chainid,
-            msg.sender,
-            expectedValidatorSequenceNumbers
-        );
+        bytes memory message = abi.encodePacked(block.chainid, msg.sender, expectedValidatorSequenceNumbers);
 
         // Aggregate the pubkeys into a single pubkey to verify the aggregated signature once
         BLS12381.G1Point memory aggPubkey = _aggregatePubkeys(pubkeys);
@@ -196,10 +176,7 @@ contract BoltValidators is IBoltValidators, BLSSignatureVerifier, Ownable {
         // Register the validators and authorize the Collateral Provider and Operator for them
         for (uint256 i = 0; i < validatorsCount; i++) {
             _registerValidator(
-                pubkeys[i],
-                expectedValidatorSequenceNumbers[i],
-                authorizedCollateralProvider,
-                authorizedOperator
+                pubkeys[i], expectedValidatorSequenceNumbers[i], authorizedCollateralProvider, authorizedOperator
             );
         }
     }
@@ -219,22 +196,15 @@ contract BoltValidators is IBoltValidators, BLSSignatureVerifier, Ownable {
         }
 
         uint256 validatorsCount = pubkeys.length;
-        uint64[] memory expectedValidatorSequenceNumbers = new uint64[](
-            validatorsCount
-        );
+        uint64[] memory expectedValidatorSequenceNumbers = new uint64[](validatorsCount);
         for (uint256 i = 0; i < validatorsCount; i++) {
-            expectedValidatorSequenceNumbers[i] =
-                nextValidatorSequenceNumber +
-                uint64(i);
+            expectedValidatorSequenceNumbers[i] = nextValidatorSequenceNumber + uint64(i);
         }
 
         // Register the validators and authorize the Collateral Provider and Operator for them
         for (uint256 i = 0; i < validatorsCount; i++) {
             _registerValidator(
-                pubkeys[i],
-                expectedValidatorSequenceNumbers[i],
-                authorizedCollateralProvider,
-                authorizedOperator
+                pubkeys[i], expectedValidatorSequenceNumbers[i], authorizedCollateralProvider, authorizedOperator
             );
         }
     }
