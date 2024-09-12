90e2b168c231b80949b356f11fc9898fab591233
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2024-09-12 10:46:23 +0200

    test(contracts): more on EL integration

--- bolt-contracts/test/BoltManager.EigenLayer.t.sol
+++ bolt-contracts/test/BoltManager.EigenLayer.t.sol
@@ -5,6 +5,8 @@ import {Test, console} from "forge-std/Test.sol";
 
 import {BoltValidators} from "../src/contracts/BoltValidators.sol";
 import {BoltManager} from "../src/contracts/BoltManager.sol";
+import {IBoltValidators} from "../src/interfaces/IBoltValidators.sol";
+import {IBoltManager} from "../src/interfaces/IBoltManager.sol";
 
 import {AVSDirectoryStorage} from "@eigenlayer/src/contracts/core/AVSDirectoryStorage.sol";
 import {DelegationManagerStorage} from "@eigenlayer/src/contracts/core/DelegationManagerStorage.sol";
@@ -17,12 +19,17 @@ import {EigenLayerDeployer} from "../test/fixtures/EigenLayerDeployer.f.sol";
 import {BLS12381} from "../src/lib/bls/BLS12381.sol";
 
 contract BoltManagerEigenLayerTest is Test {
+    using BLS12381 for BLS12381.G1Point;
+
+    uint48 public constant EPOCH_DURATION = 1 days;
+
     BoltValidators public validators;
     BoltManager public manager;
     EigenLayerDeployer public eigenLayerDeployer;
 
     address staker = makeAddr("staker");
     address validator = makeAddr("validator");
+    BLS12381.G1Point validatorPubkey = BLS12381.generatorG1();
     address operator;
     uint256 operatorSk;
 
@@ -52,11 +59,22 @@ contract BoltManagerEigenLayerTest is Test {
         );
     }
 
-    function _eigenLayerOptInRoutine() internal {
+    function _adminRoutine() internal {
         // PART 0: Admin setup -- Collateral whitelist
         vm.startPrank(admin);
-        manager.addWhitelistedEigenLayerCollateral(address(eigenLayerDeployer.weth()));
+        manager.addWhitelistedEigenLayerCollateral(
+            address(eigenLayerDeployer.weth())
+        );
         vm.stopPrank();
+        assertEq(manager.getWhitelistedEigenLayerCollaterals().length, 1);
+        assertEq(
+            manager.getWhitelistedEigenLayerCollaterals()[0],
+            address(eigenLayerDeployer.weth())
+        );
+    }
+
+    function _eigenLayerOptInRoutine() internal {
+        _adminRoutine();
 
         // PART 1: External EigenLayer opt-in to BOLT AVS
 
@@ -64,21 +82,38 @@ contract BoltManagerEigenLayerTest is Test {
         // After this, I get back some shares that I can use at a later time for withdrawal
 
         vm.startPrank(staker);
-        eigenLayerDeployer.weth().approve(address(eigenLayerDeployer.strategyManager()), 1 ether);
-        uint256 shares = eigenLayerDeployer.strategyManager().depositIntoStrategy(
-            eigenLayerDeployer.wethStrat(), eigenLayerDeployer.weth(), 1 ether
+        eigenLayerDeployer.weth().approve(
+            address(eigenLayerDeployer.strategyManager()),
+            1 ether
         );
+        uint256 shares = eigenLayerDeployer
+            .strategyManager()
+            .depositIntoStrategy(
+                eigenLayerDeployer.wethStrat(),
+                eigenLayerDeployer.weth(),
+                1 ether
+            );
         vm.stopPrank();
-        assertEq(eigenLayerDeployer.wethStrat().sharesToUnderlyingView(shares), 1 ether);
+        assertEq(
+            eigenLayerDeployer.wethStrat().sharesToUnderlyingView(shares),
+            1 ether
+        );
 
         // 2. As a Operator, I register myself into EigenLayer using DelegationManager.registerAsOperator.
         // Note that this function doesn’t require specifying anything related to the service I’m going to provide.
         // However, a parameter describes who can delegate to me whether it can be anyone or a subset of stakers.
 
-        IDelegationManager.OperatorDetails memory operatorDetails =
-            IDelegationManager.OperatorDetails(address(0), address(0), 0);
+        IDelegationManager.OperatorDetails
+            memory operatorDetails = IDelegationManager.OperatorDetails(
+                address(0),
+                address(0),
+                0
+            );
         vm.startPrank(operator);
-        eigenLayerDeployer.delegationManager().registerAsOperator(operatorDetails, "https://boltprotocol.xyz");
+        eigenLayerDeployer.delegationManager().registerAsOperator(
+            operatorDetails,
+            "https://boltprotocol.xyz"
+        );
         vm.stopPrank();
 
         // 3. As a staker, I can start delegating funds to these operators using
@@ -86,12 +121,22 @@ contract BoltManagerEigenLayerTest is Test {
         // to delegate my funds
 
         // NOTE: this signature is not used since the operator allows funds delegated from anyone
-        ISignatureUtils.SignatureWithExpiry memory signature = ISignatureUtils.SignatureWithExpiry(bytes(""), 0);
-        console.logAddress(eigenLayerDeployer.delegationManager().delegatedTo(staker));
+        ISignatureUtils.SignatureWithExpiry memory signature = ISignatureUtils
+            .SignatureWithExpiry(bytes(""), 0);
+        console.logAddress(
+            eigenLayerDeployer.delegationManager().delegatedTo(staker)
+        );
         vm.startPrank(staker);
-        eigenLayerDeployer.delegationManager().delegateTo(operator, signature, bytes32(0));
+        eigenLayerDeployer.delegationManager().delegateTo(
+            operator,
+            signature,
+            bytes32(0)
+        );
         vm.stopPrank();
-        assertEq(eigenLayerDeployer.delegationManager().delegatedTo(staker), operator);
+        assertEq(
+            eigenLayerDeployer.delegationManager().delegatedTo(staker),
+            operator
+        );
 
         // 4. As an AVS developer I create an entrypoint contract.
         // Upon deploying the contract it is required to make a call to EL’s
@@ -114,59 +159,173 @@ contract BoltManagerEigenLayerTest is Test {
         // the operator is considered REGISTERED in a mapping avsOperatorStatus[msg.sender][operator].
 
         // Calculate the digest hash
-        bytes32 operatorRegistrationDigestHash = eigenLayerDeployer.avsDirectory()
+        bytes32 operatorRegistrationDigestHash = eigenLayerDeployer
+            .avsDirectory()
             .calculateOperatorAVSRegistrationDigestHash({
-            operator: operator,
-            avs: address(manager),
-            salt: bytes32(0),
-            expiry: UINT256_MAX
-        });
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(operatorSk, operatorRegistrationDigestHash);
+                operator: operator,
+                avs: address(manager),
+                salt: bytes32(0),
+                expiry: UINT256_MAX
+            });
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            operatorSk,
+            operatorRegistrationDigestHash
+        );
         bytes memory operatorRawSignature = abi.encodePacked(r, s, v);
-        ISignatureUtils.SignatureWithSaltAndExpiry memory operatorSignature =
-            ISignatureUtils.SignatureWithSaltAndExpiry(operatorRawSignature, bytes32(0), UINT256_MAX);
+        ISignatureUtils.SignatureWithSaltAndExpiry
+            memory operatorSignature = ISignatureUtils
+                .SignatureWithSaltAndExpiry(
+                    operatorRawSignature,
+                    bytes32(0),
+                    UINT256_MAX
+                );
         vm.expectEmit(true, true, true, true);
         emit IAVSDirectory.OperatorAVSRegistrationStatusUpdated(
-            operator, address(manager), IAVSDirectory.OperatorAVSRegistrationStatus.REGISTERED
+            operator,
+            address(manager),
+            IAVSDirectory.OperatorAVSRegistrationStatus.REGISTERED
         );
         manager.registerEigenLayerOperatorToAVS(operator, operatorSignature);
-        assertEq(manager.checkIfEigenLayerOperatorRegisteredToAVS(operator), true);
+        assertEq(
+            manager.checkIfEigenLayerOperatorRegisteredToAVS(operator),
+            true
+        );
 
         // PART 2: Validator and proposer opt into BOLT manager
         //
         // 1. --- Register Validator in BoltValidators ---
 
         // pubkeys aren't checked, any point will be fine
-        BLS12381.G1Point memory pubkey = BLS12381.generatorG1();
+        validatorPubkey = BLS12381.generatorG1();
 
         vm.prank(validator);
-        validators.registerValidatorUnsafe(pubkey, staker, operator);
-        assertEq(validators.getValidatorByPubkey(pubkey).exists, true);
-        assertEq(validators.getValidatorByPubkey(pubkey).authorizedOperator, operator);
-        assertEq(validators.getValidatorByPubkey(pubkey).authorizedCollateralProvider, staker);
+        validators.registerValidatorUnsafe(validatorPubkey, staker, operator);
+        assertEq(validators.getValidatorByPubkey(validatorPubkey).exists, true);
+        assertEq(
+            validators.getValidatorByPubkey(validatorPubkey).authorizedOperator,
+            operator
+        );
+        assertEq(
+            validators
+                .getValidatorByPubkey(validatorPubkey)
+                .authorizedCollateralProvider,
+            staker
+        );
 
         // 2. --- Operator and strategy registration into BoltManager (middleware) ---
 
         manager.registerEigenLayerOperator(operator);
         assertEq(manager.isEigenLayerOperatorEnabled(operator), true);
 
-        manager.registerEigenLayerStrategy(address(eigenLayerDeployer.wethStrat()));
-        assertEq(manager.isEigenLayerStrategyEnabled(address(eigenLayerDeployer.wethStrat())), true);
+        manager.registerEigenLayerStrategy(
+            address(eigenLayerDeployer.wethStrat())
+        );
+        assertEq(
+            manager.isEigenLayerStrategyEnabled(
+                address(eigenLayerDeployer.wethStrat())
+            ),
+            true
+        );
     }
 
     function test_deregisterEigenLayerOperatorFromAVS() public {
         _eigenLayerOptInRoutine();
         vm.prank(operator);
         manager.deregisterEigenLayerOperatorFromAVS();
-        assertEq(manager.checkIfEigenLayerOperatorRegisteredToAVS(operator), false);
+        assertEq(
+            manager.checkIfEigenLayerOperatorRegisteredToAVS(operator),
+            false
+        );
     }
 
     function test_getEigenLayerOperatorStake() public {
         _eigenLayerOptInRoutine();
 
-        uint256 amount = manager.getEigenLayerOperatorStake(operator, address(eigenLayerDeployer.weth()));
-        uint256 totalStake = manager.getEigenLayerTotalStake(2, address(eigenLayerDeployer.weth()));
+        uint256 amount = manager.getEigenLayerOperatorStake(
+            operator,
+            address(eigenLayerDeployer.weth())
+        );
+        uint256 totalStake = manager.getEigenLayerTotalStake(
+            2,
+            address(eigenLayerDeployer.weth())
+        );
         assertEq(amount, 1 ether);
         assertEq(totalStake, 1 ether);
+    }
+
+    function test_getEigenLayerProposerStatus() public {
+        _eigenLayerOptInRoutine();
+
+        bytes32 pubkeyHash = _pubkeyHash(validatorPubkey);
+
+        BoltManager.ProposerStatus memory status = manager
+            .getEigenLayerProposerStatus(pubkeyHash);
+        assertEq(status.pubkeyHash, pubkeyHash);
+        assertEq(status.operator, operator);
+        assertEq(status.active, true);
+        assertEq(status.collaterals.length, 1);
+        assertEq(status.amounts.length, 1);
+        assertEq(status.collaterals[0], address(eigenLayerDeployer.weth()));
+        assertEq(status.amounts[0], 1 ether);
+    }
+
+    function testProposersLookaheadStatus() public {
+        // This also opts in the operator which is needed
+        _eigenLayerOptInRoutine();
+        bytes32[] memory pubkeyHashes = new bytes32[](10);
+
+        // register 10 proposers with random pubkeys
+        for (uint256 i = 0; i < 10; i++) {
+            BLS12381.G1Point memory pubkey = BLS12381.generatorG1();
+            pubkey.x[0] = pubkey.x[0] + i + 2;
+            pubkey.y[0] = pubkey.y[0] + i + 2;
+
+            pubkeyHashes[i] = _pubkeyHash(pubkey);
+            validators.registerValidatorUnsafe(pubkey, staker, operator);
+        }
+
+        BoltManager.ProposerStatus[] memory statuses = manager
+            .getEigenLayerProposersStatus(pubkeyHashes);
+        assertEq(statuses.length, 10);
+    }
+
+    function testGetNonExistentProposerStatus() public {
+        _eigenLayerOptInRoutine();
+
+        bytes32 pubkeyHash = bytes32(uint256(1));
+
+        vm.expectRevert(IBoltValidators.ValidatorDoesNotExist.selector);
+        manager.getEigenLayerProposerStatus(pubkeyHash);
+    }
+
+    function testGetWhitelistedCollaterals() public {
+        _adminRoutine();
+        address[] memory collaterals = manager
+            .getWhitelistedEigenLayerCollaterals();
+        assertEq(collaterals.length, 1);
+        assertEq(collaterals[0], address(eigenLayerDeployer.weth()));
+    }
+
+    function testNonWhitelistedCollateral() public {
+        _adminRoutine();
+        vm.startPrank(admin);
+        manager.removeWhitelistedEigenLayerCollateral(
+            address(eigenLayerDeployer.weth())
+        );
+        vm.stopPrank();
+
+        address strat = address(eigenLayerDeployer.wethStrat());
+        vm.startPrank(admin);
+        vm.expectRevert(IBoltManager.CollateralNotWhitelisted.selector);
+        manager.registerEigenLayerStrategy(strat);
+        vm.stopPrank();
+    }
+
+    /// @notice Compute the hash of a BLS public key
+    function _pubkeyHash(
+        BLS12381.G1Point memory _pubkey
+    ) internal pure returns (bytes32) {
+        uint256[2] memory compressedPubKey = _pubkey.compress();
+        return keccak256(abi.encodePacked(compressedPubKey));
     }
 }
