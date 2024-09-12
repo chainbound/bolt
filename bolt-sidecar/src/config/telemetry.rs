3807669f0ebbf0b02d6583190917a07a53081d7d
Jonas Bostoen <jonas.bostoen@hotmail.com>
2024-09-12 11:10:08 +0200

    refactor(config): prefix envs

--- bolt-sidecar/src/config/telemetry.rs
+++ bolt-sidecar/src/config/telemetry.rs
@@ -3,8 +3,8 @@ use clap::Parser;
 #[derive(Parser, Debug, Clone)]
 pub struct TelemetryOpts {
     /// The port on which to expose Prometheus metrics
-    #[clap(short, long, env = "METRICS_PORT", default_value_t = 3300)]
+    #[clap(short, long, env = "BOLT_SIDECAR_METRICS_PORT", default_value_t = 3300)]
     pub metrics_port: u16,
-    #[clap(short, long, env = "DISABLE_METRICS", default_value_t = false)]
+    #[clap(short, long, env = "BOLT_SIDECAR_DISABLE_METRICS", default_value_t = false)]
     pub disable_metrics: bool,
 }
