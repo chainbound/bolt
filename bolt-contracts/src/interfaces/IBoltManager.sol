a0acabaf9f3160bee8d172ebed2074e2764f0ca1
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2024-09-23 14:07:07 +0200

    chore(contracts): BoltManager.sol remove old symbiotic functionalities, now in middleware

--- bolt-contracts/src/interfaces/IBoltManager.sol
+++ bolt-contracts/src/interfaces/IBoltManager.sol
@@ -18,12 +18,7 @@ interface IBoltManager {
     error NotRegistered();
     error OperatorNotOptedIn();
     error NotOperator();
-    error NotVault();
     error CollateralNotWhitelisted();
-    error UnknownSlasherType();
-    error SlashAmountTooHigh();
-    error StrategyNotAllowed();
-    error OperatorNotRegisteredToAVS();
 
     function getEpochStartTs(
         uint48 epoch
@@ -35,61 +30,5 @@ interface IBoltManager {
 
     function getCurrentEpoch() external view returns (uint48);
 
-    function addWhitelistedSymbioticCollateral(
-        address collateral
-    ) external;
-
-    function removeWhitelistedSymbioticCollateral(
-        address collateral
-    ) external;
-
-    function getWhitelistedSymbioticCollaterals() external view returns (address[] memory);
-
-    function isSymbioticCollateralWhitelisted(
-        address collateral
-    ) external view returns (bool);
-
-    function registerSymbioticOperator(
-        address operator
-    ) external;
-
-    function pauseSymbioticOperator() external;
-
-    function unpauseSymbioticOperator() external;
-
-    function registerSymbioticVault(
-        address vault
-    ) external;
-
-    function pauseSymbioticVault() external;
-
-    function unpauseSymbioticVault() external;
-
-    function isSymbioticVaultEnabled(
-        address vault
-    ) external view returns (bool);
-
-    function isSymbioticOperatorEnabled(
-        address operator
-    ) external view returns (bool);
-
-    function getSymbioticProposersStatus(
-        bytes32[] memory pubkeyHashes
-    ) external view returns (ProposerStatus[] memory);
-
-    function getSymbioticProposerStatus(
-        bytes32 pubkeyHash
-    ) external view returns (ProposerStatus memory);
-
     function isOperatorAuthorizedForValidator(address operator, bytes32 pubkeyHash) external view returns (bool);
-
-    function getSymbioticOperatorStake(address operator, address collateral) external view returns (uint256);
-
-    function getSymbioticOperatorStakeAt(
-        address operator,
-        address collateral,
-        uint48 timestamp
-    ) external view returns (uint256);
-
-    function getSymbioticTotalStake(uint48 epoch, address collateral) external view returns (uint256);
 }
