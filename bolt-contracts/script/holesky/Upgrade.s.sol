260900e4cf38f004f669a94371558060897735bc
Jonas Bostoen <jonas.bostoen@hotmail.com>
2024-10-17 11:58:08 +0200

    feat(contracts): add Upgrade script (wip)

---
+++ bolt-contracts/script/holesky/Upgrade.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {Script, console} from "forge-std/Script.sol";
+
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {Upgrades} from "@openzeppelin-foundry-upgrades/src/Upgrades.sol";
+
+import {BoltParametersV1} from "../../../src/contracts/BoltParametersV1.sol";
+import {BoltValidatorsV1} from "../../../src/contracts/BoltValidatorsV1.sol";
+import {BoltManagerV1} from "../../../src/contracts/BoltManagerV1.sol";
+import {BoltEigenLayerMiddlewareV1} from "../../../src/contracts/BoltEigenLayerMiddlewareV1.sol";
+import {BoltSymbioticMiddlewareV1} from "../../../src/contracts/BoltSymbioticMiddlewareV1.sol";
+import {BoltConfig} from "../../../src/lib/Config.sol";
+
+contract UpgradeBolt is Script {
+    function run() public {}
+}
