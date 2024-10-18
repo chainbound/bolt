7e814a7dc152ed99f48cec0161cc090994b27cec
nicolas <48695862+merklefruit@users.noreply.github.com>
2024-10-18 18:18:25 +0200

    feat: parse decompressed pubkeys

--- bolt-contracts/script/holesky/validators/RegisterValidators.s.sol
+++ bolt-contracts/script/holesky/validators/RegisterValidators.s.sol
@@ -38,17 +38,12 @@ contract RegisterValidators is Script {
         string memory path = string.concat(root, "/config/holesky/register_validators.json");
         string memory json = vm.readFile(path);
 
-        config.boltValidators = vm.parseJsonAddress(json, "boltValidators");
-        config.authorizedOperator = vm.parseJsonAddress(json, "authorizedOperator");
-        config.maxCommittedGasLimit = uint128(vm.parseJsonUint(json, "maxCommittedGasLimit"));
+        config.boltValidators = vm.parseJsonAddress(json, ".boltValidators");
+        config.authorizedOperator = vm.parseJsonAddress(json, ".authorizedOperator");
+        config.maxCommittedGasLimit = uint128(vm.parseJsonUint(json, ".maxCommittedGasLimit"));
 
-        string[] memory pubkeysRaw = vm.parseJsonStringArray(json, "pubkeys");
-        BLS12381.G1Point[] memory pubkeys = new BLS12381.G1Point[](pubkeysRaw.length);
-        for (uint256 i = 0; i < pubkeysRaw.length; i++) {
-            uint256[2] memory x = [uint256(0), uint256(0)];
-            uint256[2] memory y = [uint256(0), uint256(0)];
-            pubkeys[i] = BLS12381.G1Point(x, y);
-        }
+        bytes memory pubkeysRaw = vm.parseJson(json, ".pubkeys");
+        BLS12381.G1Point[] memory pubkeys = abi.decode(pubkeysRaw, (BLS12381.G1Point[]));
         config.pubkeys = pubkeys;
     }
 }
