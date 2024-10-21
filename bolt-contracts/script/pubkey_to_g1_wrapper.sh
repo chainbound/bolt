b536d08de92c7063ea3b3c90bc28cc330c2827d3
Jonas Bostoen <jonas.bostoen@hotmail.com>
2024-10-21 13:56:15 +0200

    docs(contracts): document compilation in pubkey_to_g1_wrapper.sh

--- bolt-contracts/script/pubkey_to_g1_wrapper.sh
+++ bolt-contracts/script/pubkey_to_g1_wrapper.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-# Only works on Linux
+# Notes on cross-compiling the binaries:
+#
+# Linux for arm64:
+# - sudo apt install gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
+# - GOOS=linux GOARCH=arm64 CC=aarch64-linux-gnu-gcc CGO_ENABLED=1 go build -o pubkey_to_g1-linux-arm64
+#
+# MacOS for amd64:
+# - GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -o pubkey_to_g1-darwin-amd64
+
+
 arch=$(uname -i 2>/dev/null) || arch=$(uname -p) 
 if [[ $OSTYPE == linux* ]]; then
     if [[ $arch == x86_64 ]]; then
