1decfe4df5687ed913dc701716a0082ba94ab269
David <3173957+0ex-d@users.noreply.github.com>
2025-01-25 11:10:44 +0000

    bolt-sidecar: revert

--- bolt-sidecar/crates/crypto/src/lib.rs
+++
@@ -1,7 +0,0 @@
-/// BLS12_381 signatures and verification functions.
-pub mod bls;
-pub use bls::SignableBLS;
-
-/// ECDSA signatures and verification functions.
-pub mod ecdsa;
-pub use ecdsa::SignerECDSA;
